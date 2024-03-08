2ca483b1ad2495d681f852eadbc3dec1b0434964
Javier Carnero <javier@carnero.net>
2024-03-08 21:21:58 +0100

    minify logic

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
+// // const github = require("@actions/github");
+var minify = require("html-minifier").minify;
+
+// Replace the content of the file with the minified HTML
+function minifyFile(filePath, config, verbose = false) {
+  const fs = require("fs");
+  const content = fs.readFileSync(filePath, "utf8");
+  const minifiedContent = minify(content, config);
+  if (verbose) {
+    console.log(`Minified ${filePath}`);
+    console.log(`     Before: ${content.length} bytes`);
+    console.log(`     After: ${minifiedContent.length} bytes`);
+  }
+  fs.writeFileSync(filePath, minifiedContent);
+}
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  // get inputs
+  const rootDir = core.getInput("rootDir");
+  const verbose =
+    core.getInput("verbose").toLowerCase() == "true" ? true : false;
+
+  // get all html files in the rootDir folder
+  const fs = require("fs");
+  const path = require("path");
+  const distPath = path.join(__dirname, rootDir);
+  const files = fs.readdirSync(distPath, { recursive: true });
+  const htmlFiles = files.filter((file) => file.endsWith(".html"));
+
+  // minify config
+  const minifyConfig = {
+    removeAttributeQuotes: true,
+  };
+
+  // minify each html file
+  htmlFiles.forEach((file) => {
+    const filePath = path.join(distPath, file);
+    minifyFile(filePath, minifyConfig, verbose);
+  });
+
+  core.setOutput("minifiedFiles", htmlFiles);
+  // // // Get the JSON webhook payload for the event that triggered the workflow
+  // // const payload = JSON.stringify(github.context.payload, undefined, 2);
+  // // console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
